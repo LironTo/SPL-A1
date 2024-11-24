fe18ae3369ac54a054b45468a8591e68bf9c3c85
Eyal Chen <cheneya@post.bgu.ac.il>
2024-11-24 20:53:13 +0000

    hey

--- src/Plan.cpp
+++ src/Plan.cpp
@@ -1,1 +1,12 @@
-#include 
+#include <iostream>
+#include "../include/Plan.h"
+
+Plan::Plan(const int planId, const Settlement &settlement, SelectionPolicy *selectionPolicy, const vector<FacilityType> &facilityOptions) : plan_id(planId), settlement(settlement), selectionPolicy(selectionPolicy), facilityOptions(facilityOptions), status(PlanStatus::AVALIABLE), life_quality_score(0), economy_score(0), environment_score(0) 
+{
+    // Initialize the scores
+    life_quality_score = 0;
+    economy_score = 0;
+    environment_score = 0;
+    
+}
+
