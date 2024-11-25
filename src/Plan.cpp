a3df2f96c5ca73669d97c1a30b743cb71ec4b804
Eyal Chen <cheneya@post.bgu.ac.il>
2024-11-25 07:32:53 +0000

    change

--- src/Plan.cpp
+++ src/Plan.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "../include/Plan.h"
 
-Plan::Plan(const int planId, const Settlement &settlement, SelectionPolicy *selectionPolicy, const vector<FacilityType> &facilityOptions) : plan_id(planId), settlement(settlement), selectionPolicy(selectionPolicy), facilityOptions(facilityOptions), status(PlanStatus::AVALIABLE), life_quality_score(0), economy_score(0), environment_score(0) 
+Plan::Plan(const int planId, const Settlement &settlement, SelectionPolicy *selectionPolicy, 
+const vector<FacilityType> &facilityOptions) : 
+plan_id(planId), settlement(&settlement), selectionPolicy(selectionPolicy), 
+facilityOptions(facilityOptions), status(PlanStatus::AVALIABLE),
+ life_quality_score(0), economy_score(0), environment_score(0) 
 {
     // Initialize the scores
     life_quality_score = 0;
@@ -9,4 +13,9 @@ Plan::Plan(const int planId, const Settlement &settlement, SelectionPolicy *sele
     environment_score = 0;
     
 }
+int getlifeQualityScore() const {
+    
+}
+
+
 
